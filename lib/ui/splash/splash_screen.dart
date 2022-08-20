079e3597fe1d27b70a173a32d9e23d6354651694
LinhNV <vietlinhtspt@gmail.com>
2022-08-21 03:48:27 +0700

    Add splash screen

--- lib/ui/splash/splash_screen.dart
+++ lib/ui/splash/splash_screen.dart
@@ -1,7 +1,8 @@
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
 import 'package:provider/provider.dart';
 
-import '../../models/user_info.dart';
 import '../../providers/providers.dart';
 
 class SplashScreen extends StatefulWidget {
@@ -23,10 +24,10 @@ class _SplashScreenState extends State<SplashScreen> {
         context.read<PodcastProvider>().fetch(context),
       ]).then(
         (value) {
-          if ((value[0] as UserInfo?)?.email != null) {
-            context.read<UserInfoProvider>().subcribe();
-          }
-          context.read<AuthProvider>().completeInitData();
+          // if ((value[0] as UserInfo?)?.email != null) {
+          //   context.read<UserInfoProvider>().subcribe();
+          // }
+          // context.read<AuthProvider>().completeInitData();
         },
       );
     });
@@ -36,11 +37,41 @@ class _SplashScreenState extends State<SplashScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Theme.of(context).colorScheme.primaryContainer,
-      body: Center(
-          child: Text(
-        'Loading !!!!!\n\n Đang tải dữ liệu',
-        style: Theme.of(context).textTheme.headline5,
-        textAlign: TextAlign.center,
+      body: SafeArea(
+          child: SizedBox(
+        width: double.infinity,
+        height: double.infinity,
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(top: 60.0, bottom: 30),
+              child: Image.asset(
+                'assets/logos/melior_rounded_logo.png',
+                width: 180,
+                height: 180,
+              ),
+            ),
+            Image.asset(
+              'assets/logos/melior_text_logo.png',
+              height: 50,
+            ),
+            const SizedBox(
+              height: 40,
+            ),
+            Lottie.asset(
+              'assets/animations/loading.json',
+              height: 100,
+            ),
+            Text(
+              'spash_screen.title'.tr(),
+              style: TextStyle(
+                color: Theme.of(context).colorScheme.onSurface,
+                fontSize: 20,
+                fontWeight: FontWeight.w400,
+              ),
+            ),
+          ],
+        ),
       )),
     );
   }
